0a6ed52b1044d6124bb99e48f19be5ecef6a74e0
Mario Ferrero <47794126+mariofdezzz@users.noreply.github.com>
2025-12-22 10:26:05 +0000

    feat: wip waterfall chart logic

--- src/composables/app-aside/request-detail-timing/use-request-timings.ts
+++ src/composables/app-aside/request-detail-timing/use-request-timings.ts
@@ -1,21 +1,21 @@
 import { useRequestStore } from '@/stores/request'
 import type { GraphQLRequest } from '@/types/graphql-request'
 import { storeToRefs } from 'pinia'
-import { computed, type Ref } from 'vue'
+import { computed, unref, type MaybeRef } from 'vue'
 
-export function useRequestTimings(request: Ref<GraphQLRequest>) {
+export function useRequestTimings(request: MaybeRef<GraphQLRequest>) {
   const requestStore = useRequestStore()
   const { timelineStartAt, timelineEndAt } = storeToRefs(requestStore)
 
-  const queueing = computed(() => request.value.timings._blocked_queueing ?? 0)
-  const stalled = computed(() => request.value.timings.blocked ?? 0)
-  const sent = computed(() => request.value.timings.send ?? 0)
-  const wait = computed(() => request.value.timings.wait)
-  const download = computed(() => request.value.timings.receive)
-  const total = computed(() => request.value.timings.total)
+  const queueing = computed(() => unref(request).timings._blocked_queueing ?? 0)
+  const stalled = computed(() => unref(request).timings.blocked ?? 0)
+  const sent = computed(() => unref(request).timings.send ?? 0)
+  const wait = computed(() => unref(request).timings.wait)
+  const download = computed(() => unref(request).timings.receive)
+  const total = computed(() => unref(request).timings.total)
 
   const requestStartedAt = computed(() => {
-    return new Date(request.value.timings.startedAt).getTime() - timelineStartAt.value.getTime()
+    return new Date(unref(request).timings.startedAt).getTime() - timelineStartAt.value.getTime()
   })
 
   const timespan = computed(() => timelineEndAt.value.getTime() - timelineStartAt.value.getTime())
---
+++ src/composables/timings/use-waterfall-chart.ts
@@ -0,0 +1,98 @@
+import { useRequestStore } from '@/stores/request'
+import type { ChartData, ChartOptions } from 'chart.js'
+import { computed, type UnwrapRef } from 'vue'
+import { useRequestTimings } from '../app-aside/request-detail-timing/use-request-timings'
+
+export function useWaterfallChart() {
+  const requestStore = useRequestStore()
+
+  const requestsTimings = computed(() =>
+    requestStore.requests
+      .map((request) => useRequestTimings(request))
+      .map((timings) => ({
+        queueing: timings.queueing.value,
+        stalled: timings.stalled.value,
+        sent: timings.sent.value,
+        wait: timings.wait.value,
+        download: timings.download.value,
+        total: timings.total.value,
+        requestStartedAt: timings.requestStartedAt.value,
+        timespan: timings.timespan.value,
+      })),
+  )
+  const timingsByStack = computed(() =>
+    requestsTimings.value
+      .toSorted((a, b) => a.requestStartedAt - b.requestStartedAt)
+      .reduce<UnwrapRef<typeof requestsTimings>[]>((acc, timing) => {
+        if (acc.length === 0) {
+          acc.push([timing])
+          return acc
+        }
+        const index = acc.findIndex(
+          (stack) => stack.at(-1)!.requestStartedAt + stack.at(-1)!.total < timing.requestStartedAt,
+        )
+
+        if (index === -1) {
+          acc.push([timing])
+        } else {
+          acc[index]!.push(timing)
+        }
+
+        return acc
+      }, []),
+  )
+  const timespan = computed(() => requestsTimings.value[0]?.timespan || 0)
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const options = computed<ChartOptions<any>>(() => ({
+    responsive: true,
+    maintainAspectRatio: false,
+    animation: false,
+    devicePixelRatio: window.devicePixelRatio,
+    hover: { mode: null },
+    plugins: {
+      legend: {
+        display: false,
+      },
+      title: {
+        display: false,
+      },
+      tooltip: { enabled: false },
+    },
+    indexAxis: 'y',
+    scales: {
+      x: {
+        min: 0,
+        max: timespan.value,
+        display: false,
+      },
+      y: {
+        display: false,
+      },
+    },
+  }))
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const data = computed<ChartData<any>>(() => ({
+    labels: [''],
+    datasets:
+      timingsByStack.value[0]
+        ?.map((timing) => [
+          {
+            borderSkipped: false,
+            data: [[0, timing.requestStartedAt]],
+            backgroundColor: 'transparent',
+            stack: 'timings',
+          },
+          {
+            borderSkipped: false,
+            data: [[0, timing.total]],
+            backgroundColor: '#38BF60',
+            stack: 'timings',
+          },
+        ])
+        .flat() ?? [],
+  }))
+
+  return { options, data }
+}
