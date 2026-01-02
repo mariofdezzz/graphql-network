d39af749553140b78f5859d079ce79140385a8eb
Mario Ferrero <47794126+mariofdezzz@users.noreply.github.com>
2026-01-02 15:53:11 +0000

    refactor: move chartjs init to file

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,12 @@ import './pollyfills/array-zip'
 import { createApp } from 'vue'
 import App from './app.vue'
 import { createPinia } from 'pinia'
-import {
-  BarElement,
-  CategoryScale,
-  Chart as ChartJS,
-  Legend,
-  LinearScale,
-  Title,
-  Tooltip,
-} from 'chart.js'
 import './plugins/monaco'
+import './plugins/chartjs'
 
 const app = createApp(App)
 const pinia = createPinia()
 
 app.use(pinia)
-
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
 app.mount('#app')
